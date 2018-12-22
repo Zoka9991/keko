49d3e33c6f152cd64622da694cd4c9e4f8c047ab
Zoka9991 <46074982+Zoka9991@users.noreply.github.com>
2018-12-22 22:17:16 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,18 +1,88 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
 
-client.on('message', msg => {
-  if (msg.content === '#test') {
-    msg.channel.send('Test Successful');
-    msg.channel.sendFile('https://media.giphy.com/media/8GY3UiUjwKwhO/giphy.gif')
-  }
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
 });
 
 
-client.login(process.env.NTI1NjE4OTkyMDU3MDI0NTIy.DwAp3g.DvsHwLBnFRpjKSyQaYpdRq-sw); 
-client2.login(process.env.NTI1NjE5NDIzMzgyMzM5NTg1.DwAp6Q.teRD9zwE5vdkZeXaLr4d3iUY4fo2); 
-client3.login(process.env.BOT_NTI1NjE5NjYxOTUwMjg3ODky.DwAp8w.5Ipdk0XpGxf9ByTKoGK0RWTsM9Y3); 
-client4.login(process.env.NTI1NjE5OTA1MDIyNzg3NTk1.DwAp-w.IBDGM1M-GixIxWtZBOxRMeKfpGs4); 
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
